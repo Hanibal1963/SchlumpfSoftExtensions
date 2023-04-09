656a5537ae43784cda0996d7358c41e27748e57a
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2023-04-09 16:28:06 +0200

    neue Methoden eingefügt und Codekorrekturen

--- ExtensionsTestProjekt/BitmapExtensionsPreviewDialog.Designer.vb
+++ ExtensionsTestProjekt/BitmapExtensionsPreviewDialog.Designer.vb
@@ -34,9 +34,16 @@ Partial Class BitmapExtensionsPreviewDialog
 		Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
 		Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
 		Me.Label4 = New System.Windows.Forms.Label()
+		Me.TextBox2 = New System.Windows.Forms.TextBox()
+		Me.Label5 = New System.Windows.Forms.Label()
+		Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+		Me.Label6 = New System.Windows.Forms.Label()
+		Me.Label7 = New System.Windows.Forms.Label()
+		Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
 		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
 		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
 		CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
 		Me.SuspendLayout()
 		'
 		'PictureBox1
@@ -130,11 +137,72 @@ Partial Class BitmapExtensionsPreviewDialog
 		Me.Label4.TabIndex = 9
 		Me.Label4.Text = "Symbolnummer"
 		'
-		'ImageExtensionsPreviewDialog
+		'TextBox2
+		'
+		Me.TextBox2.Location = New System.Drawing.Point(301, 45)
+		Me.TextBox2.Multiline = True
+		Me.TextBox2.Name = "TextBox2"
+		Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+		Me.TextBox2.Size = New System.Drawing.Size(290, 195)
+		Me.TextBox2.TabIndex = 10
+		Me.TextBox2.WordWrap = False
+		'
+		'Label5
+		'
+		Me.Label5.AutoSize = True
+		Me.Label5.Location = New System.Drawing.Point(298, 18)
+		Me.Label5.Name = "Label5"
+		Me.Label5.Size = New System.Drawing.Size(74, 13)
+		Me.Label5.TabIndex = 11
+		Me.Label5.Text = "Base64 Code:"
+		'
+		'PictureBox3
+		'
+		Me.PictureBox3.BackColor = System.Drawing.SystemColors.ActiveBorder
+		Me.PictureBox3.Location = New System.Drawing.Point(610, 47)
+		Me.PictureBox3.Name = "PictureBox3"
+		Me.PictureBox3.Size = New System.Drawing.Size(192, 192)
+		Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox3.TabIndex = 12
+		Me.PictureBox3.TabStop = False
+		'
+		'Label6
+		'
+		Me.Label6.AutoSize = True
+		Me.Label6.Location = New System.Drawing.Point(607, 18)
+		Me.Label6.Name = "Label6"
+		Me.Label6.Size = New System.Drawing.Size(75, 13)
+		Me.Label6.TabIndex = 13
+		Me.Label6.Text = "Bild aus Code:"
+		'
+		'Label7
+		'
+		Me.Label7.AutoSize = True
+		Me.Label7.Location = New System.Drawing.Point(21, 252)
+		Me.Label7.Name = "Label7"
+		Me.Label7.Size = New System.Drawing.Size(80, 13)
+		Me.Label7.TabIndex = 15
+		Me.Label7.Text = "Bild in Website:"
+		'
+		'WebBrowser1
+		'
+		Me.WebBrowser1.Location = New System.Drawing.Point(24, 277)
+		Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+		Me.WebBrowser1.Name = "WebBrowser1"
+		Me.WebBrowser1.Size = New System.Drawing.Size(261, 210)
+		Me.WebBrowser1.TabIndex = 16
+		'
+		'BitmapExtensionsPreviewDialog
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(302, 257)
+		Me.ClientSize = New System.Drawing.Size(821, 499)
+		Me.Controls.Add(Me.WebBrowser1)
+		Me.Controls.Add(Me.Label7)
+		Me.Controls.Add(Me.Label6)
+		Me.Controls.Add(Me.PictureBox3)
+		Me.Controls.Add(Me.Label5)
+		Me.Controls.Add(Me.TextBox2)
 		Me.Controls.Add(Me.Label4)
 		Me.Controls.Add(Me.NumericUpDown1)
 		Me.Controls.Add(Me.Button2)
@@ -148,13 +216,14 @@ Partial Class BitmapExtensionsPreviewDialog
 		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
 		Me.MaximizeBox = False
 		Me.MinimizeBox = False
-		Me.Name = "ImageExtensionsPreviewDialog"
+		Me.Name = "BitmapExtensionsPreviewDialog"
 		Me.ShowInTaskbar = False
-		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
 		Me.Text = "ImageExtensionsPreviewDialog"
 		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
 		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
 		CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
 		Me.ResumeLayout(False)
 		Me.PerformLayout()
 
@@ -172,4 +241,10 @@ Partial Class BitmapExtensionsPreviewDialog
 	Friend WithEvents OpenFileDialog1 As Windows.Forms.OpenFileDialog
 	Friend WithEvents NumericUpDown1 As Windows.Forms.NumericUpDown
 	Friend WithEvents Label4 As Windows.Forms.Label
+	Friend WithEvents TextBox2 As Windows.Forms.TextBox
+	Friend WithEvents Label5 As Windows.Forms.Label
+	Friend WithEvents PictureBox3 As Windows.Forms.PictureBox
+	Friend WithEvents Label6 As Windows.Forms.Label
+	Friend WithEvents Label7 As Windows.Forms.Label
+	Friend WithEvents WebBrowser1 As Windows.Forms.WebBrowser
 End Class
